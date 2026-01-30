b591f00a68ebef554208fd9ac05bfc0db1f7d5b1
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2026-01-30 17:28:52 +0300

    feat: initialize ParamEditor state from props
    - map params to paramValues with existing values from model
    - fallback to empty string for new params

--- src/ParamEditor.tsx
+++ src/ParamEditor.tsx
@@ -27,6 +27,23 @@ interface State {
 }
 
 export default class ParamEditor extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+
+    const initialParamValues = props.params.map((param) => {
+      const existingValue = props.model.paramValues.find(
+        (paramValue) => paramValue.paramId === param.id,
+      );
+
+      return {
+        paramId: param.id,
+        value: existingValue ? existingValue.value : "",
+      };
+    });
+
+    this.state = { paramValues: initialParamValues };
+  }
+
   public getModel(): Model {
     return {
       paramValues: [],
